70df3a2c798c7bc59086299f53dbec841e10fa26
Peter Jang <peter.x.jang@gmail.com>
2024-07-10 14:03:22 -0500

    Change individual product routes to a single route using params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,33 +4,8 @@ class ProductsController < ApplicationController
     render template: "products/index"
   end
 
-  def first_product
-    @product = Product.first
-    render template: "products/show"
-  end
-
-  def second_product
-    @product = Product.second
-    render template: "products/show"
-  end
-
-  def third_product
-    @product = Product.third
-    render template: "products/show"
-  end
-
-  def fourth_product
-    @product = Product.fourth
-    render template: "products/show"
-  end
-
-  def fifth_product
-    @product = Product.fifth
-    render template: "products/show"
-  end
-
-  def sixth_product
-    @product = Product.find_by(id: 6)
+  def one_product
+    @product = Product.find_by(id: params["id"])
     render template: "products/show"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/all_products", controller: "products", action: "all_products"
-  get "/first_product", controller: "products", action: "first_product"
-  get "/second_product", controller: "products", action: "second_product"
-  get "/third_product", controller: "products", action: "third_product"
-  get "/fourth_product", controller: "products", action: "fourth_product"
-  get "/fifth_product", controller: "products", action: "fifth_product"
-  get "/sixth_product", controller: "products", action: "sixth_product"
-  get "/seventh_product", controller: "products", action: "seventh_product"
+  get "/one_product/:id", controller: "products", action: "one_product"
 end
