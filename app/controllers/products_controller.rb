07e43a0fd88864fa8957f3cff3b684fd418a1c85
Peter Jang <peter.x.jang@gmail.com>
2024-07-10 13:53:37 -0500

    Add routes for each product (annoying)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,34 @@ class ProductsController < ApplicationController
     @products = Product.all
     render template: "products/index"
   end
+
+  def first_product
+    @product = Product.first
+    render template: "products/show"
+  end
+
+  def second_product
+    @product = Product.second
+    render template: "products/show"
+  end
+
+  def third_product
+    @product = Product.third
+    render template: "products/show"
+  end
+
+  def fourth_product
+    @product = Product.fourth
+    render template: "products/show"
+  end
+
+  def fifth_product
+    @product = Product.fifth
+    render template: "products/show"
+  end
+
+  def sixth_product
+    @product = Product.find_by(id: 6)
+    render template: "products/show"
+  end
 end
