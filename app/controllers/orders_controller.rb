669e991e32b856b0399291a7e6f176e6c578fa8d
Peter Jang <peter.x.jang@gmail.com>
2024-07-30 13:55:54 -0500

    Fix orders create to use calculations

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,13 +5,24 @@ class OrdersController < ApplicationController
   end
 
   def create
+    # Find the product with an id of params[:product_id]
+    # Then grab its price
+    # Then multiply it by params[:quantity]
+    product = Product.find_by(id: params[:product_id])
+    price = product.price
+    calculated_subtotal = price * params[:quantity].to_i
+    # Multiply the subtotal by the tax rate
+    calculated_tax = calculated_subtotal * 0.09
+    # Add the subtotal and tax
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.create(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
     render :show
   end
