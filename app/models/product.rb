1258e252f0e57f946931bd66ae3d683e63b2a794
Peter Jang <peter.x.jang@gmail.com>
2024-08-09 12:33:55 -0500

    Add has many through associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,12 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
+  # def categories
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end
+  # end
 
   def is_discounted?
     price <= 10
